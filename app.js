e29e31ec46ef0cf3ed7a5040000846ef96de0db8
Mustafa Madkour <madkour2790@gmail.com>
2020-03-11 17:21:54 +0200

    intial

---
+++ app.js
@@ -0,0 +1,26 @@
+const posts = [
+    {title: 'Post 1', body: 'this is post one'},
+    {title: 'Post 2', body: 'this is post two'}
+];
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}: ${post.body}</li>`;
+        })
+        document.querySelector('.replace_post').innerHTML = output;
+    }, 5000);
+}
+
+getPosts();
+
+async function fetchUsers() {
+    let userList = '';
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    data.forEach((user, index) => {
+        userList += `<li>${user.name}</li>`;
+    })
+    document.querySelector('.replace_user').innerHTML = userList;
+};
+fetchUsers();
